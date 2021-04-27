0732c07f37d8b1580cd31a919c68e79d5cd2d83f
Diego Luques <luquesdiego@outlook.com>
2021-04-26 22:06:38 -0400

    API em um novo controlador

---
+++ src/TodoList.WebAPI.WebApp/Api/LivrosController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoList.ListaLeitura.Modelos;
+using TodoList.ListaLeitura.Persistencia;
+
+namespace TodoList.WebAPI.WebApp.Api
+{
+	public class LivrosController : Controller
+	{
+		private readonly IRepository<Livro> _repository;
+
+		public LivrosController(IRepository<Livro> repository)
+		{
+			_repository = repository;
+		}
+
+		[HttpGet]
+		public IActionResult Recuperar(int id)
+		{
+			var model = _repository.Find(id);
+			if (model == null)
+				return NotFound();
+
+			return Json(model.ToModel());
+		}
+	}
+}
